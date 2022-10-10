d912a401f825d74a382f0e7e01670d55b7259088
ahkamulbd <ahkamulbd@gmail.com>
2022-10-10 16:24:40 +0600

    Added event listener and compare email password according to Lecture 26-3

---
+++ js/login.js
@@ -0,0 +1,22 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+    //console.log('Event is active')
+    // Step 2: get the email address inside the email input field
+    // always remember to use .value to get text from an input field
+
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+
+    // Step 3: get password
+
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+
+    // Do not verify email password on the client side
+
+    if (email === 'ami@ark.com' && password === '12345') {
+        console.log('Valid User');
+    }
+    else {
+        console.log('Invalid User');
+    }
+})
