55bd2e0667cd53fa9bb0f4460a095a9e66535d2e
ahkamulbd <ahkamulbd@gmail.com>
2022-10-12 07:19:07 +0600

    Final touch done in Baap Er Bank

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,26 +5,40 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
-    //console.log(newWithdrawAmount);
+    console.log(newWithdrawAmount);
+
+    // Step 7
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid number');
+        return;
+    }
 
     // Step 3
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    // Step 4
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+
 
     // Step 5
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Baap Er Bank-e eto taka nai');
+        return;
+    }
+
+    // Step 4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     // Step 6
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
-    // Step 7
-    withdrawField.value = '';
+
 })
